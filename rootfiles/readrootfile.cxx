1ed6bb56b592dc447a1c8f056876fd21749db170
lstorchi <loriano@storchi.org>
2014-08-26 11:15:01 +0200

    extract all values

--- rootfiles/readrootfile.cxx
+++ rootfiles/readrootfile.cxx
@@ -27,39 +27,44 @@ void readandtest (const std::string & fname)
   t1->Print();
   std::cout << std::endl;
 
-  /*
-  TT->SetBranchAddress("STUB_n", &m_stub);
-  TT->SetBranchAddress("STUB_modid", &p_m_stub_modid);
-  TT->SetBranchAddress("STUB_ptGEN", &p_m_stub_ptGEN);
-  TT->SetBranchAddress("STUB_phiGEN", &p_m_stub_phiGEN);
-  TT->SetBranchAddress("STUB_etaGEN", &p_m_stub_etaGEN);
-  TT->SetBranchAddress("STUB_x", &p_m_stub_x);
-  TT->SetBranchAddress("STUB_y", &p_m_stub_y);
-  TT->SetBranchAddress("STUB_z", &p_m_stub_z);
-  TT->SetBranchAddress("STUB_pdgGEN", &p_m_stub_pdg);
-  TT->SetBranchAddress("STUB_xGEN", &p_m_stub_x0);
-  TT->SetBranchAddress("STUB_yGEN", &p_m_stub_y0);
-  TT->SetBranchAddress("STUB_zGEN", &p_m_stub_z0);
-  */
-
   std::vector<int> layerid, * p_layerid, moduleid, * p_moduleid, 
     ladderid, * p_ladderid, tp, * p_tp;
-  std::vector<float> stubx, * p_stubx, stuby, * p_stuby, stubz, * p_stubz;
   p_layerid = &layerid;
   p_ladderid = &ladderid;
   p_moduleid = &moduleid;
   p_tp = &tp;
-  p_stubx = &stubx;
-  p_stuby = &stuby;
-  p_stubz = &stubz;
+
   TT->SetBranchAddress("L1TkSTUB_layer", &p_layerid);
   TT->SetBranchAddress("L1TkSTUB_ladder", &p_ladderid);
   TT->SetBranchAddress("L1TkSTUB_module", &p_moduleid);
   TT->SetBranchAddress("L1TkSTUB_tp", &p_tp);
 
+  std::vector<float> stubx, * p_stubx, stuby, * p_stuby, stubz, * p_stubz,
+    px, * p_px, py, * p_py, x0, * p_x0, y0, * p_y0, z0, * p_z0, eta, * p_eta,
+    phi, * p_phi;
+  p_stubx = &stubx;
+  p_stuby = &stuby;
+  p_stubz = &stubz;
+  p_px = &px;
+  p_py = &py;
+  p_x0 = &x0;
+  p_y0 = &y0;
+  p_z0 = &z0;
+  p_eta = &eta;
+  p_phi = &phi;
+
   TT->SetBranchAddress("L1TkSTUB_x", &p_stubx);
   TT->SetBranchAddress("L1TkSTUB_y", &p_stuby);
   TT->SetBranchAddress("L1TkSTUB_z", &p_stubz);
+
+  TT->SetBranchAddress("L1TkSTUB_pxGEN", &p_px);
+  TT->SetBranchAddress("L1TkSTUB_pyGEN", &p_py);
+  TT->SetBranchAddress("L1TkSTUB_X0", &p_x0);
+  TT->SetBranchAddress("L1TkSTUB_Y0", &p_y0);
+  TT->SetBranchAddress("L1TkSTUB_Z0", &p_z0);
+  TT->SetBranchAddress("L1TkSTUB_etaGEN", &p_eta);
+  TT->SetBranchAddress("L1TkSTUB_PHI0", &p_phi);
+
 
   unsigned int countevt = 0;
   Int_t nevent = t1->GetEntries(); 
@@ -76,9 +81,27 @@ void readandtest (const std::string & fname)
      assert (layerid.size() == ladderid.size());
      assert (layerid.size() == moduleid.size());
      assert (layerid.size() == tp.size());
+     
      assert (layerid.size() == stubx.size());
      assert (layerid.size() == stuby.size());
      assert (layerid.size() == stubz.size());
+     assert (layerid.size() == px.size());
+     assert (layerid.size() == py.size());
+     assert (layerid.size() == x0.size());
+     assert (layerid.size() == y0.size());
+     assert (layerid.size() == z0.size());
+     assert (layerid.size() == eta.size());
+     assert (layerid.size() == phi.size());
+
+     /*
+      *
+      * pT = sqrt(pxGEN^2 + pyGEN^2)
+      * Phi = PHI0 
+      * d0 = sqrt(X0^2 + Y0^2)
+      * Eta = etaGEN
+      * z0 = Z0
+      *
+      */
 
      if (layerid.size() == 6)
      {
@@ -87,7 +110,7 @@ void readandtest (const std::string & fname)
        for (int j=0; j<(int)layerid.size(); ++j)
        {
          std::cout << stubx[j] << " " << stuby[j] << " " <<
-           stubz[j] ;
+           stubz[j];
          std::cout << layerid[j] << " " << ladderid[j] << " " << 
            moduleid[j] << " "  << tp[j] << std::endl;
        }
