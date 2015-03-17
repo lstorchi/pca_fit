6e5d806eb1acf67db50988890d6d9106bcb89c2b
lstorchi <loriano@storchi.org>
2015-03-17 18:19:25 +0100

    iminor

--- bin/analyse_results.sh
+++ bin/analyse_results.sh
@@ -3,28 +3,40 @@ if [ -z "$1" ]; then
   exit;
 fi
 
+VALDPT=80
+
 tail -n +2 $1 | awk '{print $1}' > onept_orig
 tail -n +2 $1 | awk '{print $2}' > onept_pred
-python histo.py onept_orig 80 >  onept_orig.hist
-python histo.py onept_pred 80 >  onept_pred.hist
+python histo.py onept_orig $VALDPT >  onept_orig.hist
+python histo.py onept_pred $VALDPT >  onept_pred.hist
+tail -n +2 $1 | awk '{print $2-$1}' > onept_diff
+python histo.py onept_diff $VALDPT > onept_diff.hist
 
 tail -n +2 $1 | awk '{print $4}' > phi_orig
 tail -n +2 $1 | awk '{print $5}' > phi_pred
-python histo.py phi_orig 80 >  phi_orig.hist
-python histo.py phi_pred 80 >  phi_pred.hist
+python histo.py phi_orig $VALDPT >  phi_orig.hist
+python histo.py phi_pred $VALDPT >  phi_pred.hist
+tail -n +2 $1 | awk '{print $5-$4}' > phi_diff
+python histo.py phi_diff $VALDPT > phi_diff.hist
 
 tail -n +2 $1 | awk '{print $7}' > eta_orig
 tail -n +2 $1 | awk '{print $8}' > eta_pred
-python histo.py eta_orig 80 >  eta_orig.hist
-python histo.py eta_pred 80 >  eta_pred.hist
+python histo.py eta_orig $VALDPT >  eta_orig.hist
+python histo.py eta_pred $VALDPT >  eta_pred.hist
+tail -n +2 $1 | awk '{print $8-$7}' > eta_diff
+python histo.py eta_diff $VALDPT > eta_diff.hist
 
 tail -n +2 $1 | awk '{print $10}' > d0_orig
 tail -n +2 $1 | awk '{print $11}' > d0_pred
-python histo.py d0_orig 80 >  d0_orig.hist
-python histo.py d0_pred 80 >  d0_pred.hist
+python histo.py d0_orig $VALDPT >  d0_orig.hist
+python histo.py d0_pred $VALDPT >  d0_pred.hist
+tail -n +2 $1 | awk '{print $11-$10}' > d0_diff
+python histo.py d0_diff $VALDPT > d0_diff.hist
 
 tail -n +2 $1 | awk '{print $13}' > z0_orig
 tail -n +2 $1 | awk '{print $14}' > z0_pred
-python histo.py z0_orig 80 >  z0_orig.hist
-python histo.py z0_pred 80 >  z0_pred.hist
+python histo.py z0_orig $VALDPT >  z0_orig.hist
+python histo.py z0_pred $VALDPT >  z0_pred.hist
+tail -n +2 $1 | awk '{print $14-$13}' > z0_diff
+python histo.py z0_diff $VALDPT > z0_diff.hist
 
