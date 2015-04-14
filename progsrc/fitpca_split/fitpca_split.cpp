83941fcab5f3d52dd00352c3cc09a4ac5f47d137
Loriano Storchi <loriano@storchi.org>
2015-04-14 16:04:52 +0200

    first fit split implementation (train way back, so check it)

--- progsrc/fitpca_split/fitpca_split.cpp
+++ progsrc/fitpca_split/fitpca_split.cpp
@@ -22,16 +22,29 @@
 
 bool build_and_compare (arma::mat & paramslt, arma::mat & coordslt, 
      arma::mat & cmtx, arma::rowvec & q, bool verbose, 
-     pca::pcafitter & fitter)
+     pca::pcafitter & fitter, bool rzplane, bool rphiplane)
 {
-  double * cotethacmp, * z0cmp;
-  cotethacmp = new double [(int)coordslt.n_rows];
-  z0cmp = new double [(int)coordslt.n_rows];
-
   double ** ptrs;
   ptrs = new double* [fitter.get_paramdim()];
-  ptrs[SPLIT_COTTETHAIDX] = cotethacmp;
-  ptrs[SPLIT_Z0IDX] = z0cmp;
+
+  double * cotethacmp, * z0cmp, * ptcmp, * phicmp;
+
+  if (rzplane)
+  {
+    cotethacmp = new double [(int)coordslt.n_rows];
+    z0cmp = new double [(int)coordslt.n_rows];
+
+    ptrs[SPLIT_COTTETHAIDX] = cotethacmp;
+    ptrs[SPLIT_Z0IDX] = z0cmp;
+  }
+  else if (rphiplane)
+  {
+    ptcmp = new double [(int)coordslt.n_rows];
+    phicmp = new double [(int)coordslt.n_rows];
+  
+    ptrs[SPLIT_PTIDX] = ptcmp;
+    ptrs[SPLIT_PHIIDX] = phicmp;
+  }
 
   if (!fitter.compute_parameters (cmtx, q, coordslt, ptrs, 
         fitter.get_paramdim()))
@@ -45,35 +58,69 @@ bool build_and_compare (arma::mat & paramslt, arma::mat & coordslt,
   std::ostringstream fname;
   fname << "results.txt";
 
-  std::ofstream myfile(fname.str().c_str());
-  myfile << "eta_orig eta_cmpt diff z0_orig z0_cmpt diff" << std::endl; 
-
   arma::running_stat<double> pc[fitter.get_paramdim()];
-  for (int i=0; i<(int)coordslt.n_rows; ++i)
+  std::ofstream myfile(fname.str().c_str());
+
+  if (rzplane)
   {
-    double tantetha = (1.0e0 / cotethacmp[i]) ; 
-    double etacmps = -1.0e0 * log (tantetha);
-    tantetha = (1.0e0 / paramslt(i, SPLIT_COTTETHAIDX));
-    double etaorig = -1.0e0 * log (tantetha); 
-  
-    pc[SPLIT_COTTETHAIDX](fabs(etacmps - etaorig)/
-        (fabs(etacmps + etaorig)/2.0));
-    pc[SPLIT_Z0IDX](fabs(z0cmp[i] - paramslt(i, SPLIT_Z0IDX))/
-        (fabs(z0cmp[i] + paramslt(i, SPLIT_Z0IDX))/2.0));
-
-    myfile << 
-      etaorig << " " << etacmps << " " <<
-      (etacmps - etaorig) << " " <<
-      paramslt(i, SPLIT_Z0IDX) << " " << z0cmp[i] << " " <<
-      (z0cmp[i] - paramslt(i, SPLIT_Z0IDX)) << std::endl;
-
-    if (verbose)
+    myfile << "eta_orig eta_cmpt diff z0_orig z0_cmpt diff" << std::endl; 
+    
+    for (int i=0; i<(int)coordslt.n_rows; ++i)
     {
-      std::cout << "For track : " << i+1 << std::endl;
-      std::cout << " eta          cmpt " << etacmps << std::endl;
-      std::cout << " eta          calc " << etaorig << std::endl;
-      std::cout << " z0           cmpt " << z0cmp[i] << std::endl;
-      std::cout << " z0           calc " << paramslt(i, SPLIT_Z0IDX) << std::endl;
+      double tantetha = (1.0e0 / cotethacmp[i]) ; 
+      double etacmps = -1.0e0 * log (tantetha);
+      tantetha = (1.0e0 / paramslt(i, SPLIT_COTTETHAIDX));
+      double etaorig = -1.0e0 * log (tantetha); 
+    
+      pc[SPLIT_COTTETHAIDX](fabs(etacmps - etaorig)/
+          (fabs(etacmps + etaorig)/2.0));
+      pc[SPLIT_Z0IDX](fabs(z0cmp[i] - paramslt(i, SPLIT_Z0IDX))/
+          (fabs(z0cmp[i] + paramslt(i, SPLIT_Z0IDX))/2.0));
+    
+      myfile << 
+        etaorig << " " << etacmps << " " <<
+        (etacmps - etaorig) << " " <<
+        paramslt(i, SPLIT_Z0IDX) << " " << z0cmp[i] << " " <<
+        (z0cmp[i] - paramslt(i, SPLIT_Z0IDX)) << std::endl;
+    
+      if (verbose)
+      {
+        std::cout << "For track : " << i+1 << std::endl;
+        std::cout << " eta          cmpt " << etacmps << std::endl;
+        std::cout << " eta          calc " << etaorig << std::endl;
+        std::cout << " z0           cmpt " << z0cmp[i] << std::endl;
+        std::cout << " z0           calc " << paramslt(i, SPLIT_Z0IDX) << std::endl;
+      }
+    }
+  }
+  else if (rphiplane)
+  {
+    std::ofstream myfile(fname.str().c_str());
+    myfile << "pt_orig pt_cmpt diff phi_orig phi_cmpt diff" << std::endl; 
+    
+    arma::running_stat<double> pc[fitter.get_paramdim()];
+    for (int i=0; i<(int)coordslt.n_rows; ++i)
+    {
+      pc[SPLIT_PHIIDX](fabs(phicmp[i] - paramslt(i, SPLIT_PHIIDX))/
+          (fabs(phicmp[i] + paramslt(i, SPLIT_PHIIDX))/2.0));
+
+      pc[SPLIT_PTIDX](fabs(1.0e0/ptcmp[i] - 1.0e0/paramslt(i, SPLIT_PTIDX))/
+          (fabs(1.0e0/ptcmp[i] + 1.0e0/paramslt(i, SPLIT_PTIDX))/2.0));
+ 
+      myfile << 
+        1.0e0/paramslt(i, SPLIT_PTIDX) << " " << 1.0e0/ptcmp[i] << " " <<
+        (1.0e0/ptcmp[i] - 1.0e0/paramslt(i, SPLIT_PTIDX)) <<  " " <<
+        paramslt(i, SPLIT_PHIIDX) << " " << phicmp[i] << " " <<
+        (phicmp[i] - paramslt(i, SPLIT_PHIIDX)) << std::endl;
+    
+      if (verbose)
+      {
+        std::cout << "For track : " << i+1 << std::endl;
+        std::cout << " pt           cmpt " << 1.0e0/ptcmp[i] << std::endl;
+        std::cout << " pt           calc " << 1.0e0/paramslt(i, SPLIT_PTIDX)  << std::endl;
+        std::cout << " phi          cmpt " << phicmp[i] << std::endl;
+        std::cout << " phi          calc " << paramslt(i, SPLIT_PHIIDX) << std::endl;
+      }
     }
   }
 
@@ -83,8 +130,16 @@ bool build_and_compare (arma::mat & paramslt, arma::mat & coordslt,
      std::cout << "For " << fitter.paramidx_to_string(i) << " error " << 
        100.0*pc[i].mean() << " " << 100.0*pc[i].stddev() << std::endl;
 
-  delete [] cotethacmp;
-  delete [] z0cmp;
+  if (rzplane)
+  {
+    delete [] cotethacmp;
+    delete [] z0cmp;
+  }
+  else if (rphiplane)
+  {
+    delete [] ptcmp;
+    delete [] phicmp;
+  }
 
   return true;
 } 
@@ -99,6 +154,8 @@ void usage (char * name)
   std::cerr << " -c, --cmtx=[fillename]   : CMTX filename [default is c.<selectedsubsecid>.bin]" << std::endl;
   std::cerr << " -q, --qvct=[fillename]   : QVCT filename [default is q.<selectedsubsecid>.bin]" << std::endl;
   std::cerr << " -j, --jump-tracks        : perform the fittin only for odd tracks" << std::endl;
+  std::cerr << " -z, --rz-plane           : use rz plane view" << std::endl;
+  std::cerr << " -r, --rphi-plane         : use r-phi plane view" << std::endl;
 
   exit(1);
 }
@@ -113,6 +170,8 @@ int main (int argc, char ** argv)
   std::string sublad = "";
   bool verbose = false;
   bool useonlyodd = false;
+  bool rzplane = false;
+  bool rphiplane = false;
 
   while (1)
   {
@@ -124,16 +183,24 @@ int main (int argc, char ** argv)
       {"verbose", 0, NULL, 'V'},
       {"version", 0, NULL, 'v'},
       {"jump-tracks", 0, NULL, 'j'},
+      {"rz-plane", 0, NULL, 'z'},
+      {"rphi-plane", 0, NULL, 'r'},
       {0, 0, 0, 0}
     };
 
-    c = getopt_long (argc, argv, "hVc:q:s:j", long_options, &option_index);
+    c = getopt_long (argc, argv, "zrhVc:q:s:j", long_options, &option_index);
 
     if (c == -1)
       break;
 
     switch (c)
     {
+      case 'z':
+        rzplane = true;
+        break;
+      case 'r':
+        rphiplane = true;
+        break;
       case 'j':
         useonlyodd = true;
         break;
@@ -159,23 +226,47 @@ int main (int argc, char ** argv)
     } 
   }
 
+  if (optind >= argc) 
+    usage (argv[0]);
+
+  if ((rzplane && rphiplane) ||
+      (!rzplane && !rphiplane))
+  {
+    std::cerr << "r-phi or r-z plane ?" << std::endl;
+    usage (argv[0]);
+  }
+
   // R-z
   fitter.set_coordim (2*6);
 
   fitter.set_paramdim(2);
-  if (!fitter.set_paramidx(SPLIT_COTTETHAIDX, "cot(tetha/2)"))
+  if (rzplane)
   {
-    std::cerr << fitter.get_errmsg() << std::endl;
-    return EXIT_FAILURE;
+    if (!fitter.set_paramidx(SPLIT_COTTETHAIDX, "cot(tetha/2)"))
+    {
+      std::cerr << fitter.get_errmsg() << std::endl;
+      return EXIT_FAILURE;
+    }
+    if (!fitter.set_paramidx(SPLIT_Z0IDX, "z0"))
+    {
+      std::cerr << fitter.get_errmsg() << std::endl;
+      return EXIT_FAILURE;
+    }
   }
-  if (!fitter.set_paramidx(SPLIT_Z0IDX, "z0"))
+  else if (rphiplane)
   {
-    std::cerr << fitter.get_errmsg() << std::endl;
-    return EXIT_FAILURE;
+    if (!fitter.set_paramidx(SPLIT_PHIIDX, "phi"))
+    {
+      std::cerr << fitter.get_errmsg() << std::endl;
+      return EXIT_FAILURE;
+    }
+    if (!fitter.set_paramidx(SPLIT_PTIDX, "1/pt"))
+    {
+      std::cerr << fitter.get_errmsg() << std::endl;
+      return EXIT_FAILURE;
+    }
   }
 
-  if (optind >= argc) 
-    usage (argv[0]);
 
   char * filename = (char *) alloca (strlen(argv[optind]) + 1);
   strcpy (filename, argv[optind]);
@@ -217,13 +308,13 @@ int main (int argc, char ** argv)
 
   pca::reading_from_file_split (filename, 
      param, coord, num_of_ent, false, useonlyodd,
-     true, false);
+     rzplane, rphiplane);
 
   pca::read_armmat(cfname.c_str(), cmtx);
   pca::read_armvct(qfname.c_str(), q);
 
   if (!build_and_compare (param, coord, cmtx, q, verbose, 
-          fitter))
+          fitter, rzplane, rphiplane))
     return EXIT_FAILURE;
 
   return EXIT_SUCCESS;
