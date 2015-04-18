9ac90bcc15d095b8f6333149a528180875aca9db
Loriano Storchi <loriano@storchi.org>
2015-04-18 13:41:23 +0200

    minor

--- bin/analyse_results_rz.sh
+++ bin/analyse_results_rz.sh
@@ -9,12 +9,12 @@ tail -n +2 $1 | awk '{print $1}' > eta_orig
 tail -n +2 $1 | awk '{print $2}' > eta_pred
 python histo.py eta_orig $VALDPT >  eta_orig.hist
 python histo.py eta_pred $VALDPT >  eta_pred.hist
-tail -n +2 $1 | awk '{print $2-$1}' > eta_diff
+tail -n +2 $1 | awk '{print $3}' > eta_diff
 python histo.py eta_diff $VALDPT > eta_diff.hist
 
 tail -n +2 $1 | awk '{print $4}' > z0_orig
 tail -n +2 $1 | awk '{print $5}' > z0_pred
 python histo.py z0_orig $VALDPT >  z0_orig.hist
 python histo.py z0_pred $VALDPT >  z0_pred.hist
-tail -n +2 $1 | awk '{print $5-$4}' > z0_diff
+tail -n +2 $1 | awk '{print $6}' > z0_diff
 python histo.py z0_diff $VALDPT > z0_diff.hist
