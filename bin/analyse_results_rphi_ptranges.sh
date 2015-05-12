44f0ba79cf3037c5959a8b6c3cf95c9e0e02cac7
Loriano Storchi <loriano@storchi.org>
2015-05-12 11:38:20 +0200

    minor

--- bin/analyse_results_rphi_ptranges.sh
+++ bin/analyse_results_rphi_ptranges.sh
@@ -12,8 +12,6 @@ python histo.py pt_pred $VALDPT >  pt_pred.hist
 tail -n +2 $1 | awk '{print ((1.0/$1 - 1.0/$2)/(1.0/$1))}' > pt_diff
 python histo.py pt_diff $VALDPT > pt_diff.hist
 
-tail -n +2 $1 | awk '{print 1.0/$1, ((1.0/$1 - 1.0/$2)/(1.0/$1))}' > pt_diff_vs_pt
-
 tail -n +2 $1 | awk '{print $4}' > phi_orig
 tail -n +2 $1 | awk '{print $5}' > phi_pred
 python histo.py phi_orig $VALDPT >  phi_orig.hist
@@ -21,6 +19,7 @@ python histo.py phi_pred $VALDPT >  phi_pred.hist
 tail -n +2 $1 | awk '{print $6}' > phi_diff
 python histo.py phi_diff $VALDPT > phi_diff.hist
 
+
+tail -n +2 $1 | awk '{print 1.0/$1, ((1.0/$1 - 1.0/$2)/(1.0/$1))}' > pt_diff_vs_pt
 tail -n +2 $1 | awk '{print $4, $6}' > phi_diff_vs_phi
 tail -n +2 $1 | awk '{print 1.0/$1, $6}' > phi_diff_vs_pt
-
