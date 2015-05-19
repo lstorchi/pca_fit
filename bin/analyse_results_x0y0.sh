b00e1d599a8af59eb19474999938d268f3e15039
Loriano Storchi <loriano@storchi.org>
2015-05-19 16:09:18 +0200

    minor

--- bin/analyse_results_x0y0.sh
+++ bin/analyse_results_x0y0.sh
@@ -13,14 +13,14 @@ fi
 
 tail -n +2 $1 | awk '{print $1}' > x0_orig
 tail -n +2 $1 | awk '{print $2}' > x0_pred
-python histo.py eta_orig $VALDPT >  x0_orig.hist
-python histo.py eta_pred $VALDPT >  x0_pred.hist
+python histo.py x0_orig $VALDPT >  x0_orig.hist
+python histo.py x0_pred $VALDPT >  x0_pred.hist
 tail -n +2 $1 | awk '{print $3}' > x0_diff
-python histo.py eta_diff $VALDPT > x0_diff.hist
+python histo.py x0_diff $VALDPT > x0_diff.hist
 
 tail -n +2 $1 | awk '{print $4}' > y0_orig
 tail -n +2 $1 | awk '{print $5}' > y0_pred
-python histo.py z0_orig $VALDPT >  y0_orig.hist
-python histo.py z0_pred $VALDPT >  y0_pred.hist
+python histo.py y0_orig $VALDPT >  y0_orig.hist
+python histo.py y0_pred $VALDPT >  y0_pred.hist
 tail -n +2 $1 | awk '{print $6}' > y0_diff
-python histo.py z0_diff $VALDPT > y0_diff.hist
+python histo.py y0_diff $VALDPT > y0_diff.hist
