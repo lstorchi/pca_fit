5a575b7848fe186c235b6d5e9a0a716811c3908f
Loriano Storchi <loriano@storchi.org>
2015-05-19 16:07:32 +0200

    minor
    Former-commit-id: 6db32e4d2a9c0a93040ada686610bc03799c48f0

---
+++ bin/analyse_results_x0y0.sh
@@ -0,0 +1,26 @@
+if [ -z "$1" ]; then 
+  echo "$0 results.file.txt"; 
+  exit;
+fi
+
+VALDPT=80
+
+if [ -z "$2" ]; then 
+  VALDPT=80
+else
+  VALDPT=$2
+fi
+
+tail -n +2 $1 | awk '{print $1}' > x0_orig
+tail -n +2 $1 | awk '{print $2}' > x0_pred
+python histo.py eta_orig $VALDPT >  x0_orig.hist
+python histo.py eta_pred $VALDPT >  x0_pred.hist
+tail -n +2 $1 | awk '{print $3}' > x0_diff
+python histo.py eta_diff $VALDPT > x0_diff.hist
+
+tail -n +2 $1 | awk '{print $4}' > y0_orig
+tail -n +2 $1 | awk '{print $5}' > y0_pred
+python histo.py z0_orig $VALDPT >  y0_orig.hist
+python histo.py z0_pred $VALDPT >  y0_pred.hist
+tail -n +2 $1 | awk '{print $6}' > y0_diff
+python histo.py z0_diff $VALDPT > y0_diff.hist
