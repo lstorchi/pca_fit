97f33627369085381a04930a44d64a1150593bae
Loriano Storchi <loriano@storchi.org>
2015-06-11 15:49:07 +0200

    add pt to results
    Former-commit-id: 803ad363c318ed111a4b4d614d6cd7453a29fdd2

--- bin/analyse_results.sh
+++ bin/analyse_results.sh
@@ -5,38 +5,38 @@ fi
 
 VALDPT=80
 
-tail -n +2 $1 | awk '{print $1}' > onept_orig
-tail -n +2 $1 | awk '{print $2}' > onept_pred
+tail -n +2 $1 | awk '{print $2}' > onept_orig
+tail -n +2 $1 | awk '{print $3}' > onept_pred
 python histo.py onept_orig $VALDPT >  onept_orig.hist
 python histo.py onept_pred $VALDPT >  onept_pred.hist
-tail -n +2 $1 | awk '{print $2-$1}' > onept_diff
+tail -n +2 $1 | awk '{print $3-$2}' > onept_diff
 python histo.py onept_diff $VALDPT > onept_diff.hist
 
-tail -n +2 $1 | awk '{print $4}' > phi_orig
-tail -n +2 $1 | awk '{print $5}' > phi_pred
+tail -n +2 $1 | awk '{print $5}' > phi_orig
+tail -n +2 $1 | awk '{print $6}' > phi_pred
 python histo.py phi_orig $VALDPT >  phi_orig.hist
 python histo.py phi_pred $VALDPT >  phi_pred.hist
-tail -n +2 $1 | awk '{print $5-$4}' > phi_diff
+tail -n +2 $1 | awk '{print $6-$5}' > phi_diff
 python histo.py phi_diff $VALDPT > phi_diff.hist
 
-tail -n +2 $1 | awk '{print $7}' > eta_orig
-tail -n +2 $1 | awk '{print $8}' > eta_pred
+tail -n +2 $1 | awk '{print $8}' > eta_orig
+tail -n +2 $1 | awk '{print $9}' > eta_pred
 python histo.py eta_orig $VALDPT >  eta_orig.hist
 python histo.py eta_pred $VALDPT >  eta_pred.hist
-tail -n +2 $1 | awk '{print $8-$7}' > eta_diff
+tail -n +2 $1 | awk '{print $9-$8}' > eta_diff
 python histo.py eta_diff $VALDPT > eta_diff.hist
 
-tail -n +2 $1 | awk '{print $10}' > d0_orig
-tail -n +2 $1 | awk '{print $11}' > d0_pred
+tail -n +2 $1 | awk '{print $11}' > d0_orig
+tail -n +2 $1 | awk '{print $12}' > d0_pred
 python histo.py d0_orig $VALDPT >  d0_orig.hist
 python histo.py d0_pred $VALDPT >  d0_pred.hist
-tail -n +2 $1 | awk '{print $11-$10}' > d0_diff
+tail -n +2 $1 | awk '{print $12-$11}' > d0_diff
 python histo.py d0_diff $VALDPT > d0_diff.hist
 
-tail -n +2 $1 | awk '{print $13}' > z0_orig
-tail -n +2 $1 | awk '{print $14}' > z0_pred
+tail -n +2 $1 | awk '{print $14}' > z0_orig
+tail -n +2 $1 | awk '{print $15}' > z0_pred
 python histo.py z0_orig $VALDPT >  z0_orig.hist
 python histo.py z0_pred $VALDPT >  z0_pred.hist
-tail -n +2 $1 | awk '{print $14-$13}' > z0_diff
+tail -n +2 $1 | awk '{print $15-$14}' > z0_diff
 python histo.py z0_diff $VALDPT > z0_diff.hist
 
--- bin/analyse_results_d0.sh
+++ bin/analyse_results_d0.sh
@@ -11,9 +11,9 @@ else
   VALDPT=$2
 fi
 
-tail -n +2 $1 | awk '{print $7}' > d0_orig
-tail -n +2 $1 | awk '{print $8}' > d0_pred
+tail -n +2 $1 | awk '{print $8}' > d0_orig
+tail -n +2 $1 | awk '{print $9}' > d0_pred
 python histo.py d0_orig $VALDPT >  d0_orig.hist
 python histo.py d0_pred $VALDPT >  d0_pred.hist
-tail -n +2 $1 | awk '{print $9}' > d0_diff
+tail -n +2 $1 | awk '{print $10}' > d0_diff
 python histo.py d0_diff $VALDPT > d0_diff.hist
