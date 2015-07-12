dee825019af79003a9a5f0a2b757cbe7408d8353
Loriano Storchi <loriano@storchi.org>
2015-07-12 13:45:06 +0200

    minor
    Former-commit-id: e043fc5b6ab5c325c89c4fbd32041ece8614ba6f

--- progsrc/utilities/reader_and_reg.py
+++ progsrc/utilities/reader_and_reg.py
@@ -1,4 +1,17 @@
 import sys
+import re
+
+####################################################################
+
+def file_len(fname):
+  
+  with open(fname) as f:
+    for i, l in enumerate(f):
+      pass
+
+  return i + 1
+
+####################################################################
 
 filename = "noname.txt"
 
@@ -8,4 +21,37 @@ if (len(sys.argv) == 1):
 else:
   filename = sys.argv[1]
 
+numofline = file_len(filename)
 
+numofline = (numofline-1) 
+
+rint 
+
+fp = open(filename, "r")
+
+# jump first line
+fp.readline()
+
+for i in range(numofline):
+  l = fp.readline()
+
+  p = re.compile(r'\s+')
+  line = p.sub(' ', l)
+  line = line.lstrip()
+  line = line.rstrip()
+           
+  plist = line.split(" ")
+
+  numof = int(plist[1])
+
+  for j in range(numof):
+    coordline = fp.readline()
+
+  paramline = fp.readline()
+
+  print paramline
+
+
+
+
+fp.close()
