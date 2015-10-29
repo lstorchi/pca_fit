6898721ea8fca1f2ba4c77307fe30782fc26dea4
Loriano Storchi <loriano@storchi.org>
2015-10-29 14:53:00 +0100

    removed "old" code from readrootfile

--- progsrc/readrootfile/readrootfile.cxx
+++ progsrc/readrootfile/readrootfile.cxx
@@ -32,17 +32,14 @@ void usage (char * name)
   std::cerr << "usage: " << name << " [options] rootfile " << std::endl;
   std::cerr << std::endl;
   std::cerr << " -h, --help               : display this help and exit" << std::endl;
-  std::cerr << " -b, --bank-stubs         : extract bankstubs" << std::endl;
-  std::cerr << " -n, --bank-stubs-new     : extract bankstubs new version" << std::endl;
-  std::cerr << " -e, --not-use-charge     : if new version is true by default read also charge" << std::endl;
   std::cerr << " -l, --l1tk-stubs         : extract l1tkstubs" << std::endl;
   std::cerr << " -m, --max-tracks[=value] : max value of tracks to be extracted" << std::endl;
 
   exit(1);
 }
 
-void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtracks,
-    bool usecharge)
+void print_bankstub_new (TFile * inputFile, std::ostream& ss, 
+    std::ostream& ssext, unsigned int maxtracks)
 {
   TChain* TT = (TChain*) inputFile->Get("BankStubs");
 
@@ -77,9 +74,7 @@ void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtr
   TT->SetBranchAddress("STUB_Z0", &p_z0);
   TT->SetBranchAddress("STUB_etaGEN", &p_eta);
   TT->SetBranchAddress("STUB_PHI0", &p_phi);
-
-  if (usecharge)
-    TT->SetBranchAddress("STUB_pdg", &p_pdg);
+  TT->SetBranchAddress("STUB_pdg", &p_pdg);
 
   unsigned int countevt = 0;
   Int_t nevent = TT->GetEntries(); 
@@ -105,8 +100,7 @@ void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtr
      assert (moduleid.size() == z0.size());
      assert (moduleid.size() == eta.size());
      assert (moduleid.size() == phi.size());
-     if (usecharge)
-       assert (moduleid.size() == pdg.size());
+     assert (moduleid.size() == pdg.size());
 
      bool allAreEqual = ((std::find_if(z0.begin() + 1, z0.end(), 
         std::bind1st(std::not_equal_to<int>(), z0.front())) == z0.end()) &&
@@ -162,12 +156,8 @@ void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtr
         value = value-module*100;
         int segid = value; // QA is just this ? from the source code seems so, I need to / by 10 ?
 
-        if (usecharge)
-          ss << layer << " " << ladder << " " << 
-            module << " " << segid << " " << pdg[j] << std::endl;
-        else
-          ss << layer << " " << ladder << " " << 
-            module << " " << segid << std::endl;
+        ss << layer << " " << ladder << " " << 
+          module << " " << segid << " " << pdg[j] << std::endl;
        }
        --j;
 
@@ -216,12 +206,8 @@ void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtr
         value = value-module*100;
         int segid = value; // QA is just this ? from the source code seems so, I need to / by 10 ?
 
-        if (usecharge)
-          ss << layer << " " << ladder << " " << 
-            module << " " << segid << " " << pdg[j] << std::endl;
-        else
-          ss << layer << " " << ladder << " " << 
-            module << " " << segid << std::endl;
+        ss << layer << " " << ladder << " " << 
+          module << " " << segid << " " << pdg[j] << std::endl;
        }
        --j;
 
@@ -231,110 +217,36 @@ void print_bankstub_new (TFile * inputFile, std::ostream& ss, unsigned int maxtr
          x0[j] << " " << y0[j] << std::endl;
 
        countevt++;
- 
      }
- 
-
-     if (countevt >= maxtracks)
-       break;
-  }
-  ptfile.close();
-  phifile.close();
-  d0file.close();
-  etafile.close();
-  z0file.close();
-}
-
-
-
-void print_bankstub (TFile * inputFile, std::ostream& ss, unsigned int maxtracks)
-{
-  TChain* TT = (TChain*) inputFile->Get("BankStubs");
-
-  std::vector<int> moduleid, * p_moduleid; 
-  p_moduleid = &moduleid;
-
-  TT->SetBranchAddress("STUB_modid", &p_moduleid); // QA come determino layerid e altro ? 
-                                                   //    devo caricare la geometria ?
-  std::vector<float> stubx, * p_stubx, stuby, * p_stuby, stubz, * p_stubz,
-    pt, * p_pt, x0, * p_x0, y0, * p_y0, z0, * p_z0, eta, * p_eta,
-    phi, * p_phi;
-  p_stubx = &stubx;
-  p_stuby = &stuby;
-  p_stubz = &stubz;
-  p_pt = &pt;
-  p_x0 = &x0;
-  p_y0 = &y0;
-  p_z0 = &z0;
-  p_eta = &eta;
-  p_phi = &phi;
-
-  TT->SetBranchAddress("STUB_x", &p_stubx);
-  TT->SetBranchAddress("STUB_y", &p_stuby);
-  TT->SetBranchAddress("STUB_z", &p_stubz);
-
-  TT->SetBranchAddress("STUB_ptGEN", &p_pt);
-  TT->SetBranchAddress("STUB_xGEN", &p_x0);
-  TT->SetBranchAddress("STUB_yGEN", &p_y0);
-  TT->SetBranchAddress("STUB_zGEN", &p_z0);
-  TT->SetBranchAddress("STUB_etaGEN", &p_eta);
-  TT->SetBranchAddress("STUB_phiGEN", &p_phi);
-
-  unsigned int countevt = 0;
-  Int_t nevent = TT->GetEntries(); 
-  ss << "We got " << nevent << " events in BankStubs" << std::endl; 
-  // QA perche' il numero di eventi qui e' molto maggiore ?
-
-  std::ofstream ptfile("pt_BankStubs.txt");
-  std::ofstream phifile("phi_BankStubs.txt");
-  std::ofstream d0file("d0_BankStubs.txt");
-  std::ofstream etafile("eta_BankStubs.txt");
-  std::ofstream z0file("z0_BankStubs.txt");
-  for (Int_t i=0; i<nevent; ++i) 
-  { 
-     TT->GetEntry(i);
-     
-     assert (moduleid.size() == stubx.size());
-     assert (moduleid.size() == stuby.size());
-     assert (moduleid.size() == stubz.size());
-     assert (moduleid.size() == pt.size());
-     assert (moduleid.size() == x0.size());
-     assert (moduleid.size() == y0.size());
-     assert (moduleid.size() == z0.size());
-     assert (moduleid.size() == eta.size());
-     assert (moduleid.size() == phi.size());
-
-     bool allAreEqual = ((std::find_if(z0.begin() + 1, z0.end(), 
-        std::bind1st(std::not_equal_to<int>(), z0.front())) == z0.end()) &&
-                        (std::find_if(x0.begin() + 1, x0.end(), 
-        std::bind1st(std::not_equal_to<int>(), x0.front())) == x0.end()) &&
-                        (std::find_if(y0.begin() + 1, y0.end(), 
-        std::bind1st(std::not_equal_to<int>(), y0.front())) == y0.end()) &&
-                        (std::find_if(pt.begin() + 1, pt.end(), 
-        std::bind1st(std::not_equal_to<int>(), pt.front())) == pt.end()) &&
-                        (std::find_if(eta.begin() + 1, eta.end(), 
-        std::bind1st(std::not_equal_to<int>(), eta.front())) == eta.end()) &&
-                        (std::find_if(phi.begin() + 1, phi.end(), 
-        std::bind1st(std::not_equal_to<int>(), phi.front())) == phi.end()));
-
-
-     if ((moduleid.size() == 6)  && allAreEqual) // QA nel caso dei BankStubs questo check e' utile ?
+     else 
      {
+       double d0val;
+       //d0val = (y0[0]-(tan(phi[0])*x0[0]))*cos(phi[0]);
+       d0val = y0[0]*cos(phi[0])-x0[0]*sin(phi[0]);
+       //double d0val = x0[0];
+
        ptfile << pt[0] << std::endl;
        phifile << phi[0] << std::endl;
-       d0file << sqrt(pow(x0[0],2.0) + pow(y0[0],2.0)) 
-         << std::endl;
+       d0file << d0val << std::endl;
        etafile << eta[0] << std::endl;
        z0file << z0[0] << std::endl;
 
-       ss << i+1 << " " << moduleid.size() << std::endl;
+       ssext << i+1 << " " << moduleid.size() << std::endl;
 
        int j = 0;
        for (; j<(int)moduleid.size(); ++j)
        {
-        ss << stubx[j] << " " << stuby[j] << " " <<
-           stubz[j] << " ";
+#ifdef INTBITEWISE
+        int16_t stubX = stubx[j]*10;
+        int16_t stubY = stuby[j]*10;
+        int16_t stubZ = stubz[j]*10;
 
+        ssext << stubX << " " << stubY << " " <<
+           stubZ << " ";
+#else
+        ssext << stubx[j] << " " << stuby[j] << " " <<
+           stubz[j] << " ";
+#endif
         int value = moduleid[j];
         int layer = value/1000000;
         value = value-layer*1000000;
@@ -344,17 +256,19 @@ void print_bankstub (TFile * inputFile, std::ostream& ss, unsigned int maxtracks
         value = value-module*100;
         int segid = value; // QA is just this ? from the source code seems so, I need to / by 10 ?
 
-        ss << layer << " " << ladder << " " << 
-          module << " " << segid << " " << std::endl;
+        ssext << layer << " " << ladder << " " << 
+          module << " " << segid << " " << pdg[j] << std::endl;
        }
        --j;
 
-       ss << pt[j]<< " "  <<
-         phi[j] << " " << sqrt(pow(x0[j],2.0) + pow(y0[j],2.0)) << " " 
-         << eta[j] << " " << z0[j] << std::endl;
+       ssext << pt[j]<< " "  <<
+         phi[j] << " " << d0val << " " 
+         << eta[j] << " " << z0[j] << " " <<
+         x0[j] << " " << y0[j] << std::endl;
 
        countevt++;
      }
+ 
 
      if (countevt >= maxtracks)
        break;
@@ -365,6 +279,7 @@ void print_bankstub (TFile * inputFile, std::ostream& ss, unsigned int maxtracks
   etafile.close();
   z0file.close();
 }
+
 
 void print_l1tkstub (TFile * inputFile, std::ostream & ss, unsigned int maxtracks)
 {
@@ -527,7 +442,7 @@ void print_l1tkstub (TFile * inputFile, std::ostream & ss, unsigned int maxtrack
 }
 
 void readandtest (const std::string & fname, bool tkstubs, 
-    bool bkstubs, bool bkstubsnew, int maxtracks, bool usecharge)
+    bool bkstubs, int maxtracks)
 {
   //TFile* inputFile = new TFile(fname.c_str(),"READ");
   // use xrootd as suggested 
@@ -588,24 +503,11 @@ void readandtest (const std::string & fname, bool tkstubs,
   if (bkstubs)
   {
     std::ofstream bankstbfile("bankstub.txt");
-    //print_bankstub (inputFile, std::cerr);
-    print_bankstub (inputFile, bankstbfile, (unsigned int)maxtracks);
-    // QA le distribuzioni dei vari parametri sono simili ma 
-    //    ancora in bankstub ci sono tantssime tracce in piu'.
-    //    ed il module id ha un valore numeri che non "capisco" immagino
-    //    sia una specie di identificativo univoco del modulo
-    //    L1TkStubs should be not there maybe is just a bad file 
-    //         there should be only BankStubs 
+    std::ofstream bankstbfileex("bankstubextra.txt");
+    print_bankstub_new (inputFile, bankstbfile, bankstbfileex, 
+        (unsigned int)maxtracks);
     bankstbfile.close();
-  }
-
-
-  if (bkstubsnew)
-  {
-    std::ofstream bankstbfile("bankstub.txt");
-    print_bankstub_new (inputFile, bankstbfile, (unsigned int)maxtracks,
-        usecharge);
-    bankstbfile.close();
+    bankstbfileex.close();
   }
 
   if (tkstubs)
@@ -622,10 +524,8 @@ void readandtest (const std::string & fname, bool tkstubs,
 # ifndef __CINT__
 int main(int argc, char ** argv) 
 {
-  bool usecharge = true;
   bool tkstubs = false;
-  bool bkstubs = false;
-  bool bkstubsnew = false;
+  bool bkstubs = true;
   int maxtracks = STOPAFTERMAXEVT;
 
   while (1)
@@ -633,15 +533,12 @@ int main(int argc, char ** argv)
     int c, option_index;
     static struct option long_options[] = {
       {"help", 0, NULL, 'h'},
-      {"bank-stubs", 0, NULL, 'b'},
-      {"bank-stubs-new", 0, NULL, 'n'},
       {"l1tk-stubs", 0, NULL, 'l'},
       {"max-track", 0, NULL, 'm'}, 
-      {"not-use-charge", 0, NULL, 'e'},
       {0, 0, 0, 0}
     };
 
-    c = getopt_long (argc, argv, "enhlbm:", long_options, &option_index);
+    c = getopt_long (argc, argv, "hlm:", long_options, &option_index);
 
     if (c == -1)
       break;
@@ -654,17 +551,8 @@ int main(int argc, char ** argv)
       case 'l':
         tkstubs = true;
         break;
-      case'b':
-        bkstubs = true;
-        break;
-      case'n':
-        bkstubsnew = true;
-        break;
       case 'm':
         maxtracks = atoi(optarg);
-        break;
-      case 'e':
-        usecharge = false;
         break;
       default:
         usage (argv[0]);
@@ -675,7 +563,7 @@ int main(int argc, char ** argv)
   if (optind >= argc) 
     usage (argv[0]);
 
-  readandtest(argv[optind], tkstubs, bkstubs, bkstubsnew, maxtracks, usecharge);
+  readandtest(argv[optind], tkstubs, bkstubs, maxtracks);
 
   return 0;
 }
