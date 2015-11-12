6d4fddca2da0c9a4c5565e2d2f3581a871d7e244
Loriano Storchi <loriano@storchi.org>
2015-11-12 21:24:31 +0100

    remove code to check
    Former-commit-id: a634ad8d25b37b40ebc934ee60b8dc06640d9915

--- src/utility.cpp
+++ src/utility.cpp
@@ -325,21 +325,6 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
      bool checklayersids,
      int maxnumoflayers)
 {
-
-#ifdef DEBUG
-  double x0min = -1.0e0 * INFINITY, x0max = +1.0e0 * INFINITY;
-  double y0min = -1.0e0 * INFINITY, y0max = +1.0e0 * INFINITY;
-
-  /*
-  x0min = -0.05;
-  x0max =  0.05;
-  y0min = -0.05;
-  y0max =  0.05;
-  */
-
-  arma::mat xyzvals;
-#endif
-
   std::vector<std::string> layersids;
 
   int extdim = 9;
@@ -366,21 +351,6 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
   d0vals.set_size(num_of_ent);
   z0vals.set_size(num_of_ent);
 
-#ifdef DEBUG
-  /* to set x0 and y0 range */
-  xyzvals.set_size(num_of_ent, 3*maxnumoflayers);
-  arma::vec x0vals;
-  arma::vec y0vals;
-  x0vals.set_size(num_of_ent);
-  y0vals.set_size(num_of_ent);
-#endif
-
-  if (useonlyeven && useonlyodd)
-  {
-    useonlyeven = false;
-    useonlyodd = false;
-  }
-
   std::ifstream mytfp;
   mytfp.open (filename, std::ios::in);
 
@@ -394,51 +364,18 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
     int fake1, realsize;
     mytfp >> fake1 >> realsize ;
 
-#if 0
-    if (realsize > maxnumoflayers)
-    {
-      std::cerr << "WARNING: More than 6 layers" << std::endl;
-      std::cerr << "         Not checking missing stub TODO" << std::endl;
-    }
-
-    if (realsize < maxnumoflayers)
-    {
-      std::cerr << "WARNING: less than ",maxnumoflayers," layers" << std::endl;
-      std::cerr << "         wont use it" << std::endl;
-      continue;
-    }
-#endif
-
     std::ostringstream osss;
     std::set<int> pidset, layeridset;
     
     for (int j = 0; j < realsize; ++j)
     {
       int a, b, c, segid, pid;
-#ifdef INTBITEWISE
-      int16_t x, y, z;
-#else
       double x, y, z;
-#endif
 
-      if (chargeoverpt)
-      {
-        mytfp >> x >> 
-                 y >> 
-                 z >> 
-                 a >> b >> c >> segid >> pid; // segid I am reading because can be used as local ccordinate ?
-                                              // in case of l1tkstubs is the tp value here 
-                                              // pid is particle id (charge)
-      }
-      else
-      {
-        mytfp >> x >> 
-                 y >> 
-                 z >> 
-                 a >> b >> c >> segid; // segid I am reading because can be used as local ccordinate ?
-                                              // in case of l1tkstubs is the tp value here 
-                                              // pid is particle id (charge)
-      }
+      mytfp >> x >> 
+               y >> 
+               z >> 
+               a >> b >> c >> segid >> pid; 
 
       osss << a;
       layeridset.insert(a);
@@ -454,48 +391,20 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
         
         if (check_charge_sign(chargesign, pidset))
         {
-          if (check_to_read (useonlyeven,useonlyodd,i))
+          double ri = sqrt(pow(x, 2.0) + pow (y, 2.0));
+
+          if (rzplane)
           {
-#ifdef INTBITEWISE
-            int16_t ri = sqrt(pow(x, 2.0) + pow (y, 2.0));
-#else
-            double ri = sqrt(pow(x, 2.0) + pow (y, 2.0));
-#endif 
-
-#ifdef DEBUG
-            xyzvals(counter, (3*j)+0) = x;
-            xyzvals(counter, (3*j)+1) = y;
-            xyzvals(counter, (3*j)+2) = z;
-#endif
-
-            if (rzplane)
-            {
-              coordread(counter, j*2) = z;
-              coordread(counter, j*2+1) = ri;
+            coordread(counter, j*2) = z;
+            coordread(counter, j*2+1) = ri;
         
-              // fake to use XY or similar plane
-              //coordread(counter, j*2) = x;
-              //coordread(counter, j*2+1) = y;
-        
-            }
-            else if (rphiplane)
-            {
-//recast x and r for arccos calculation. Though arccos operation not permitted in integer representation.
-//Check X-Y view instead
-#ifdef INTBITEWISE
-              int16_t phii = 10*acos((double) x/(double) ri);
-#else
-              double phii = acos(x/ri);
-#endif
+          }
+          else if (rphiplane)
+          {
+            double phii = acos(x/ri);
           
-              coordread(counter, j*2) = phii;
-              coordread(counter, j*2+1) = ri;
-
-              // quick switch to XY view
-              //coordread(counter, j*2) = x;
-              //coordread(counter, j*2+1) = y;
- 
-            }
+            coordread(counter, j*2) = phii;
+            coordread(counter, j*2+1) = ri;
           }
         }
       }
@@ -504,7 +413,6 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
     if (layeridset.size() != (unsigned int)realsize)
     {
       ++countlayerswithdupid;
-      //std::cerr << "Layer id duplicated " << std::endl;
     }
 
     //Need to change for Integer Representation , but for the time 
@@ -520,138 +428,44 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
              x0read >>
              y0read;
 
-    if (check_to_read (useonlyeven,useonlyodd,i))
+    layersids.push_back(osss.str());
+    etavals(counter) = etaread;
+    phivals(counter) = phiread;
+    ptvals(counter) = ptread;
+    z0vals(counter) = z0read;
+    d0vals(counter) = d0read;
+
+    if (rzplane)
     {
-      layersids.push_back(osss.str());
-      etavals(counter) = etaread;
-      phivals(counter) = phiread;
-      ptvals(counter) = ptread;
-      z0vals(counter) = z0read;
-      d0vals(counter) = d0read;
-
-#ifdef DEBUG
-      x0vals(counter) = x0read;
-      y0vals(counter) = y0read;
-#endif
-
-      if ((singleparam >= 1) && (singleparam <= 7))
-      {
-        if (check_charge_sign(chargesign, pidset))
-        {
-          switch (singleparam)
-          {
-            case (1):
-              paramread(counter, 0) =  cot(2.0 * atan (exp (-1.0e0 * etaread)));
-              break;
-            case (2):
-              if (chargeoverpt)
-              {
-                if (pidset.size() != 1)
-                {
-                  std::cerr << "pid values differ" << std::endl;
-                  return false;
-                }
-             
-                if (*(pidset.begin()) < 0)
-                  paramread(counter, 0) = -1.0e0 / ptread;
-                else
-                  paramread(counter, 0) = 1.0e0 / ptread;
-              }
-              else
-                paramread(counter, 0) = 1.0e0 / ptread;
- 
-              break;
-            case (3):
-              paramread(counter, 0) = z0read;
-              break;
-            case (4):
-              paramread(counter, 0) = phiread;
-              break;
-            case (5):
-              paramread(counter, 0) = x0read;
-              break;
-            case (6):
-              paramread(counter, 0) = y0read;
-              break;
-            case (7):
-              paramread(counter, 0) = d0read;
-              break;
-            default:
-              paramread(counter, 0) = 0.0e0;
-              std::cerr << "wrong paramindex value" << std::endl;
-              break;
-          }
-        }
-      }
-      else
-      {
-        if (rzplane)
-        {
-          if (usex0y0)
-          {
-            paramread(counter, SPLIT_X0IDX) = x0read;
-            paramread(counter, SPLIT_Y0IDX) = y0read;
-          }
-          else
-          {
-            paramread(counter, SPLIT_Z0IDX) = z0read;
-            // lstorchi: I use this to diretcly convert input parameters into
-            //     better parameters for the fitting 
-            // eta = -ln[tan(theta / 2)]
-            // theta = 2 * arctan (e^(-eta))
-            // cotan (theta) = cotan (2 * arctan (e^(-eta)))
-            paramread(counter, SPLIT_COTTHETAIDX) =  cot(2.0 * atan (exp (-1.0e0 * etaread)));
-            //double theta = atan(1.0 /  paramread(counter, SPLIT_COTTHETAIDX));
-            //std::cout << etaread << " " << theta * (180/M_PI) << std::endl;
-            //just to visualize pseudorapidity 
-          }
-        
-          if (usealsod0)
-            paramread(counter, SPLIT_D0IDX) = d0read;
-          else if (usealsox0)
-            paramread(counter, SPLIT_X0IDX_NS) = x0read;
-        }
-        else if (rphiplane)
-        {
-          if (check_charge_sign(chargesign, pidset))
-          {
-            if (usex0y0)
-            {
-              paramread(counter, SPLIT_X0IDX) = x0read;
-              paramread(counter, SPLIT_Y0IDX) = y0read;
-            }
-            else
-            {
-              paramread(counter, SPLIT_PHIIDX) = phiread;
-              // use 1/pt
-              if (chargeoverpt)
-              {
-                if (pidset.size() != 1)
-                {
-                  std::cerr << "pid values differ" << std::endl;
-                  return false;
-                }
-             
-                if (*(pidset.begin()) < 0)
-                  paramread(counter, SPLIT_ONEOVERPTIDX) = -1.0e0 / ptread;
-                else
-                  paramread(counter, SPLIT_ONEOVERPTIDX) = 1.0e0 / ptread;
-              }
-              else
-                paramread(counter, SPLIT_ONEOVERPTIDX) = 1.0e0 / ptread;
-            }
-        
-            if (usealsod0)
-              paramread(counter, SPLIT_D0IDX) = d0read;
-            else if (usealsox0)
-              paramread(counter, SPLIT_X0IDX_NS) = x0read;
-          }
-        }
-      }
-
-      if (check_charge_sign(chargesign, pidset))
-        ++counter;
+      paramread(counter, SPLIT_Z0IDX) = z0read;
+      paramread(counter, SPLIT_COTTHETAIDX) =  cot(2.0 * atan (exp (-1.0e0 * etaread)));
     }
+    else if (rphiplane)
+    {
+      if (check_charge_sign(chargesign, pidset))
+      {
+        paramread(counter, SPLIT_PHIIDX) = phiread;
+        // use 1/pt
+        if (chargeoverpt)
+        {
+          if (pidset.size() != 1)
+          {
+            std::cerr << "pid values differ" << std::endl;
+            return false;
+          }
+        
+          if (*(pidset.begin()) < 0)
+            paramread(counter, SPLIT_ONEOVERPTIDX) = -1.0e0 / ptread;
+          else
+            paramread(counter, SPLIT_ONEOVERPTIDX) = 1.0e0 / ptread;
+        }
+        else
+          paramread(counter, SPLIT_ONEOVERPTIDX) = 1.0e0 / ptread;
+      }
+    }
+
+    if (check_charge_sign(chargesign, pidset))
+      ++counter;
   }
 
   assert (coordread.n_rows == paramread.n_rows);
@@ -662,11 +476,6 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
   assert (d0vals.n_rows == paramread.n_rows);
   assert (layersids.size() == paramread.n_rows);
 
-#ifdef DEBUG  
-  assert (x0vals.n_rows == paramread.n_rows);
-  assert (y0vals.n_rows == paramread.n_rows);
-#endif
-
   extdim = 0;
   for (int i=0; i<(int)etavals.n_rows; ++i)
     if (is_a_valid_layers_seq(layersids[i], checklayersids))
@@ -675,11 +484,7 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
           if ((phivals(i) <= phimax) && (phivals(i) >= phimin))
             if ((d0vals(i) <= d0max) && (d0vals(i) >= d0min))
               if ((z0vals(i) <= z0max) && (z0vals(i) >= z0min))
-#ifdef DEBUG              
-                if ((x0vals(i) <= x0max) && (x0vals(i) >= x0min))
-                  if ((y0vals(i) <= y0max) && (y0vals(i) >= y0min))
-#endif
-                    ++extdim;
+                ++extdim;
 
   coordin.resize(extdim, fitter.get_coordim());
   paramin.resize(extdim, fitter.get_paramdim());
@@ -702,47 +507,24 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
             {
               if ((z0vals(i) <= z0max) && (z0vals(i) >= z0min))
               {
-#ifdef DEBUG              
-                if ((x0vals(i) <= x0max) && (x0vals(i) >= x0min))
+                if (verbose)
                 {
-                  if ((y0vals(i) <= y0max) && (y0vals(i) >= y0min))
-                  {
-#endif
-                    if (verbose)
-                    {
-                      std::cout << "ETA : " << etavals(i) << std::endl;
-                      std::cout << "PT  : " << ptvals(i) << std::endl;
-                      std::cout << "PHI : " << phivals(i) << std::endl;
-                      std::cout << "D0  : " << d0vals(i) << std::endl;
-                      std::cout << "Z0  : " << z0vals(i) << std::endl;
-                    }
-                    
-                    for (int j=0; j<(int)paramread.n_cols; ++j)
-                    {
-                      paramin(counter, j) = paramread(i, j);
-                      //std::cout << "Track: " << counter+1 << std::endl;
-                      //std::cout <<  paramin(counter, j) << " from " << 
-                      //  paramread(i, j) << std::endl;
-                    }
-                    
-                    for (int j=0; j<(int)coordread.n_cols; ++j)
-                      coordin(counter, j) = coordread(i, j);
-                   
-                    ptvalsout(counter) = ptvals(i);
-                   
-#ifdef DEBUG                    
-                    //for (int j=0; j<maxnumoflayers; ++j)
-                    for (int j=0; j<1; ++j)
-                      std::cerr << xyzvals(i,(3*j)+0) << " " << xyzvals(i,(3*j)+1) << " " 
-                        << xyzvals(i,(3*j)+2) << std::endl;
-#endif             
-                   
-                    
-                    ++counter;
-#ifdef DEBUG                  
-                  }
+                  std::cout << "ETA : " << etavals(i) << std::endl;
+                  std::cout << "PT  : " << ptvals(i) << std::endl;
+                  std::cout << "PHI : " << phivals(i) << std::endl;
+                  std::cout << "D0  : " << d0vals(i) << std::endl;
+                  std::cout << "Z0  : " << z0vals(i) << std::endl;
                 }
-#endif
+                
+                for (int j=0; j<(int)paramread.n_cols; ++j)
+                  paramin(counter, j) = paramread(i, j);
+                
+                for (int j=0; j<(int)coordread.n_cols; ++j)
+                  coordin(counter, j) = coordread(i, j);
+                
+                ptvalsout(counter) = ptvals(i);
+                
+                ++counter;
               }
             }
           }
@@ -751,8 +533,6 @@ bool pca::reading_from_file_split (const pca::pcafitter & fitter,
     }
   }
 
-  //std::cout << "counter: " << counter << std::endl;
-  
 
   std::cout << "Layers IDs list: " << std::endl;
   std::set<int>::iterator lids = layeridlist.begin();
