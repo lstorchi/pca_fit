22b5b9da01a5af2f802e395e6fb99d2cb9877da0
Loriano Storchi <loriano@storchi.org>
2015-12-22 11:18:06 +0100

    add basic PCA trackfitter interface
    Former-commit-id: 8a84c7c860bca6a870e40978c21605899f4dc615

---
+++ CMSSW/src/L1Trigger/TrackFindingAM/interface/PCATrackFitter.h
@@ -0,0 +1,82 @@
+#ifndef _PCATRACKFITTER_H_
+#define _PCATRACKFITTER_H_
+
+#include "TrackFitter.h"
+
+#include <set>
+#include <map>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/export.hpp>
+
+/**
+   \brief Implementation of a PCA track fitter
+**/
+class PCATrackFitter:public TrackFitter{
+
+ private:
+
+  unsigned int verboseLevel; 
+  unsigned int event_counter;
+  unsigned int road_id;
+
+  // This contains the retina configuration parameters,
+  // it is filled in initialize():
+  std::map <const char*,double> config[3];
+
+  // This is used to rotate the trigger tower into the first quadrant:
+  const static double rot_angle[8];
+
+  void initialize();
+  void rotateHits(vector<Hit_t*> hits, double angle);
+  void confTrans(vector<Hit_t*> hits);
+
+  friend class boost::serialization::access;
+  
+  template<class Archive> void save(Archive & ar, const unsigned int version) const
+    {
+      ar << boost::serialization::base_object<TrackFitter>(*this);
+      ar << sec_phi;
+    }
+  
+  template<class Archive> void load(Archive & ar, const unsigned int version)
+    {
+      ar >> boost::serialization::base_object<TrackFitter>(*this);
+      ar >> sec_phi;
+    }
+  
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+ public:
+
+  /**
+     \brief Default constructor   
+  **/
+  PCATrackFitter();
+
+  /**
+     \brief Constructor   
+     \param nb Layers number
+  **/  
+  PCATrackFitter(int nb);
+
+  ~PCATrackFitter();
+
+  void mergePatterns();
+  void mergeTracks();
+  void fit();
+  void fit(std::vector<Hit*> hits);
+  TrackFitter* clone();
+
+  void setEventCounter(unsigned int event_counter_){
+    event_counter = event_counter_;
+  };
+  void setRoadID(unsigned int road_id_){
+    road_id = road_id_;
+  };
+  void setVerboseLevel(unsigned int verboseLevel_){
+    verboseLevel = verboseLevel_;
+  }; 
+
+};
+#endif
