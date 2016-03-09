e3c1b8175d6ebd2e18528a018ff81f39a1d68228
Loriano Storchi <loriano@storchi.org>
2016-03-09 14:21:43 +0100

    minor++

--- include/version.h
+++ include/version.h
@@ -8,7 +8,7 @@
  *      distributiong some test exe
  * */
 #define PCAFITTER_MAJOR_VER 0
-#define PCAFITTER_MINOR_VER 6
+#define PCAFITTER_MINOR_VER 7
 #define PCAFITTER_PATCH_VER 0
 
 #define PCAFITTER_VERSION ( PCAFITTER_MAJOR_VER * 10000 \
