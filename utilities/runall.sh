4673d42b519e7ed772cb80f1c20a7b134bab4fa4
Loriano Storchi <loriano@storchi.org>
2016-04-18 10:11:28 +0200

    minor

--- utilities/runall.sh
+++ utilities/runall.sh
@@ -9,3 +9,15 @@ do
   ./check_barrell_fast.sh $FFILE $CFILE -0.6 $num 1> out 2> err &
   cd ../ 
 done
+
+for num in 24 25 26 27 28 29 30 31
+do 
+  mkdir $num
+  cd $num
+  ln -s ../fitpca_split .
+  ln -s ../check_barrell_fast.sh .
+  export FFILE="/pub/redo/MUBANK/MUBANK_pt2To200_tow"$num".root"
+  export CFILE="../../files/barrel_tow"$num"_pca_const.txt"
+  ./check_barrell_fast.sh $FFILE $CFILE -0.4 $num 1> out 2> err &
+  cd ../ 
+done
