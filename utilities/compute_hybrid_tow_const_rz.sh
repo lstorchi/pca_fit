f5cb4ec567237d6078c6e267da61674efe47cd90
Loriano Storchi <loriano@storchi.org>
2016-08-18 10:04:08 +0200

    minor

--- utilities/compute_hybrid_tow_const_rz.sh
+++ utilities/compute_hybrid_tow_const_rz.sh
@@ -1,6 +1,8 @@
 export TOWERID=8
 export ROTTFILENAME=MUBANK_pt2To200_tow8.root
 
+rm -f pca_const.txt
+
 export k=-1.7
 for i in `seq 1 26`;
 do
@@ -13,10 +15,10 @@ do
  ./generatepca_split -x -k -R 1 --rz-plane --pt-range="3.0;200.0" --eta-range="$rage" -D $TOWERID $ROTTFILENAME
  ./fitpca_split_single -c "pca_const.txt" -x -k -R 1 --rz-plane --pt-range="3.0;200.0" --eta-range="$rage" -D $TOWERID $ROTTFILENAME
 
- rm pca_const.txt
+ rm -f pca_const.txt
 
  ./generatepca_split -k -R 1 --rz-plane --pt-range="3.0;200.0" --eta-range="$rage" -D $TOWERID $ROTTFILENAME
  ./fitpca_split_single -c "pca_const.txt" -k -R 1 --rz-plane --pt-range="3.0;200.0" --eta-range="$rage" -D $TOWERID $ROTTFILENAME
 
- rm pca_const.txt
+ rm -f pca_const.txt
 done
