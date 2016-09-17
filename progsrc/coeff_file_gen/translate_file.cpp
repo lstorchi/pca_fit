e37646158de5387957f87f247b6619011361692d
Loriano Storchi <loriano@storchi.org>
2016-09-17 06:43:54 +0200

    very first imple of flot to int translator

--- progsrc/coeff_file_gen/translate_file.cpp
+++ progsrc/coeff_file_gen/translate_file.cpp
@@ -26,50 +26,175 @@ int main (int argc, char *argv[])
   if (argc != 2)
   {
     std::cerr << "usage: " << argv[0] << " pca_const_file.txt " << std::endl;
-    return 1;
+    return EXIT_FAILURE;
   }
+
+  std::string out_fname;
 
   std::vector< matrixpcaconst<double> > all_constants;
   bool a = read_pcaconst_from_file<double>(all_constants, argv[1]);
 
-  // write rphi param
-  std::vector<double> constants_rphi_cmtx;
+  if (!a)
+  {
+    std::cerr << "Error while reading from " << argv[1] << std::endl;
+    return EXIT_FAILURE; 
+  }
+
+  out_fname = argv[1];
+  out_fname.append ("_int");
+
+  fstream fin(out_fname.c_str());
+  if (fin)
+  {
+    std::cout << "File " << out_fname << " found, deleting..." << std::endl;
+    fin.close();
+
+    remove(out_fname.c_str());
+  } 
+  
   for (matrixpcaconst<double> c : all_constants) 
   {
+    matrixpcaconst<long long int> ci(c.n_rows(), c.n_cols());
+
+    ci.set_const_type(c.get_const_type());
+    ci.set_towerid(c.get_towerid());
+    ci.set_layersids(c.get_layersids().c_str());
+    ci.set_sector_type(c.get_sector_type());
+    ci.set_plane_type(c.get_plane_type());
+    ci.set_chargesign(c.get_chargesign());
+
+    double ptmin, ptmax, etamin, etamax;
+    c.get_ptrange(ptmin, ptmax);
+    c.get_etarange(etamin, etamax);
+    ci.set_ptrange(ptmin, ptmax);
+    ci.set_etarange(etamin, etamax);
+
+    ci.set_ttype(ttype::INTEGPT);
+
     if (c.get_plane_type() == plane_type::RPHI)
     {
       if (c.get_const_type() == const_type::CMTX) 
       {
+        int counter = 0;
 	for (int i = 0; i < c.n_rows(); ++i)
         {
 	  for (int j = 0; j < c.n_cols(); ++j)
           {
-	    if ( (get_missing_layer(c) >= 0) &&
-		 (get_missing_layer(c) <= (j/2))
-		 )
-	      constants_rphi_cmtx.push_back(((get_missing_layer(c) == (j/2))?0:c.element(i, j-2)));
-	    else
-	      constants_rphi_cmtx.push_back(c.element(i, j));
+            long long int tmp = mult_factor*c.element(i,j);
+            /* TODO: unclear ask, where counter is the global index of element */
+            if ( counter%2 == 0) tmp /= 64;
+            check_val(tmp, const_w, "rphi matrix consts");
+            ci.element(i, j) = tmp;
+            counter++;
           }
         }
       }
-    }
-  }
-
-  std::vector<double> constants_rphi_qvec;
-  for (matrixpcaconst<double> c : all_constants) 
-  {
-    if (c.get_plane_type() == plane_type::RPHI)
-    {
-      if (c.get_const_type() == const_type::QVEC) 
+      else if (c.get_const_type() == const_type::QVEC)
       {
 	for (int i = 0; i < c.n_cols(); ++i)
         {
-	  constants_rphi_qvec.push_back(c.element(0, i));
+          long long int tmp = const_mult_factor*c.element(0,i);
+          check_val (tmp, add_const_w, "rphi add consts");
+          ci.element(0, i) = tmp;
         }
       }
+      else if (c.get_const_type() == const_type::AMTX) 
+      {
+        int counter = 0;
+	for (int i = 0; i < c.n_rows(); ++i)
+        {
+	  for (int j = 0; j < c.n_cols(); ++j)
+          {
+            long long int tmp = chisq_mult_factor*c.element(i,j);
+            /* TODO: unclear ask, where counter is the global index of element */
+            if ( counter%2 == 0) tmp /= 64;
+            check_val(tmp, const_w, "rphi matrix consts");
+            ci.element(i, j) = tmp;
+            counter++;
+          }
+        }
+      }
+      else if (c.get_const_type() == const_type::KVEC)
+      {
+	for (int i = 0; i < c.n_cols(); ++i)
+        {
+          long long int tmp = chisq_const_mult_factor*c.element(0,i);
+          check_val (tmp, add_const_w, "rphi add consts");
+          ci.element(0, i) = tmp;
+        }
+      }
+      else
+      {
+        std::cerr << "Unknown Const Type" << std::endl;
+        return EXIT_FAILURE;
+      }
     }
+    else if (c.get_plane_type() == plane_type::RZ)
+    {
+      if (c.get_const_type() == const_type::CMTX) 
+      {
+        int counter = 0;
+	for (int i = 0; i < c.n_rows(); ++i)
+        {
+	  for (int j = 0; j < c.n_cols(); ++j)
+          {
+            long long int tmp = mult_factor*c.element(i,j);
+            /* TODO: unclear ask, where counter is the global index of element */
+            if ( counter%2 == 0) tmp *= 4;
+            check_val(tmp, const_w, "rz matrix consts");
+            ci.element(i, j) = tmp;
+            counter++;
+          }
+        }
+      }
+      else if (c.get_const_type() == const_type::QVEC)
+      {
+	for (int i = 0; i < c.n_cols(); ++i)
+        {
+          long long int tmp = const_mult_factor*c.element(0,i);
+          check_val (tmp, add_const_w, "rz add consts");
+          ci.element(0, i) = tmp;
+        }
+      }
+      else if (c.get_const_type() == const_type::AMTX) 
+      {
+        int counter = 0;
+	for (int i = 0; i < c.n_rows(); ++i)
+        {
+	  for (int j = 0; j < c.n_cols(); ++j)
+          {
+            long long int tmp = chisq_mult_factor*c.element(i,j);
+            /* TODO: unclear ask, where counter is the global index of element */
+            if ( counter%2 == 0) tmp *= 4;
+            check_val(tmp, const_w, "rz matrix consts");
+            ci.element(i, j) = tmp;
+            counter++;
+          }
+        }
+      }
+      else if (c.get_const_type() == const_type::KVEC)
+      {
+	for (int i = 0; i < c.n_cols(); ++i)
+        {
+          long long int tmp = chisq_const_mult_factor*c.element(0,i);
+          check_val (tmp, add_const_w, "rz add consts");
+          ci.element(0, i) = tmp;
+        }
+      }
+      else
+      {
+        std::cerr << "Unknown Const Type" << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+    else 
+    {
+      std::cerr << "Unknown Plane Type" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    write_pcaconst_to_file (ci, out_fname.c_str());
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
