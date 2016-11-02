4a3e560af2a36773fa87ba555439e54c5cc466f5
Loriano Storchi <loriano@storchi.org>
2016-11-02 10:34:47 +0100

    minor

--- include/pcaconst.hpp
+++ include/pcaconst.hpp
@@ -709,7 +709,7 @@ namespace pca
           {
             if (actuallayids.find(pslayersid) != actuallayids.end())
             {
-              if ((eta >= etamin) && (eta <= etamax)) 
+              if ((eta > etamin) && (eta <= etamax)) 
               {
                 switch(it->get_const_type())
                 {
@@ -745,7 +745,7 @@ namespace pca
             {
               if (chargesignin == chargesign)
               {
-                if ((pt >= ptmin) && (pt <= ptmax))
+                if ((pt > ptmin) && (pt <= ptmax))
                 {
                   switch(it->get_const_type())
                   {
@@ -837,11 +837,11 @@ namespace pca
             {
               if (actuallayids.find(layersid) != actuallayids.end())
               {
-                if ((eta >= etamin) && (eta <= etamax)) 
+                if ((eta > etamin) && (eta <= etamax)) 
                 {
                   if (chargesignin == chargesign)
                   {
-                    if ((pt >= ptmin) && (pt <= ptmax))
+                    if ((pt > ptmin) && (pt <= ptmax))
                     {
                       switch(it->get_const_type())
                       {
